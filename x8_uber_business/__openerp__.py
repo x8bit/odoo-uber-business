e907175b34d6075e95d59664f428ae66b483b079
DezkaReid <elmaildeldezkareid@gmail.com>
2017-01-25 01:09:23 -0600

    Módulo base

---
+++ x8_uber_business/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': "X8 Uber Business - Módulo de administración de Uber Business",
+    'version': '1.0.0',
+    'author': "X8BIT SA DE CV",
+    'website': 'www.x8bit.com',
+    'category': 'Specific Industry Applications',
+    'application': True,
+    'description': """
+    X8 Uber Business - Módulo de administración de Uber Business
+
+        - Administración de lista de empleados que usan Uber, códigos y datos de viajes.
+    """,
+    'depends': [
+        'base',
+    ],
+}
